ceb74765f33631f51905d39f0497ce1aeb0fb1b2
Anna Kudriavtsev <anna328p@gmail.com>
2023-12-01 17:59:48 -0600

    add nix stuff

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ stdenv
+, lib
+,
+}:
+
+stdenv.mkDerivation {
+	pname = "TODO";
+	version = "0";
+
+	nativeBuildInputs = [
+
+	];
+
+	buildInputs = [
+
+	];
+
+	meta = {
+		maintainers = [ lib.maintainers.anna328p ];
+	};
+}
